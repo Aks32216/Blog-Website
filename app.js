4e4932755284ee60cbb221c0cc60afbd3c2048e9
Aks32216 <amishsingh568@gmail.com>
2021-11-13 06:13:45 +0530

    Added posting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -37,6 +38,10 @@ app.get("/compose",(req,res)=>{
   res.render("compose",{year:year});
 })
 
+// app.get("/post",(req,res)=>{
+//   res.render("post",{year:year,element})
+// })
+
 
 app.post("/compose",(req,res)=>{
   let post={
@@ -48,21 +53,14 @@ app.post("/compose",(req,res)=>{
 })
 
 app.get("/post/:userPost",(req,res)=>{
-  let title=req.params.userPost;
-  title.toLowerCase();
-  console.log(title);
+  let title=_.lowerCase(req.params.userPost);
   for(let i=0;i<posts.length;++i)
   {
-    let postsTitle=posts[i].title;
-    postsTitle.toLowerCase();
-    console.log(postsTitle);
+    let postsTitle=_.lowerCase(posts[i].title);
     if(postsTitle===title){
-      console.log("Match Found");
-      return;
+      res.render("post",{year:year,element:posts[i]})
     }
   }
-  console.log("Match not Found");
-
 })
 
 app.listen(3000, function() {
