0217f9b45e95e732dd11ebac533e8329c1b4fcdf
Aks32216 <amishsingh568@gmail.com>
2021-12-17 15:44:01 +0530

    added backend

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongose=require("mongoose");
 
+mongose.connect("mongodb://127.0.0.1:27017/blogWebsite",{useNewUrlParser:true})
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactStartingContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const itemSchema={
+  title:String,
+  body:String  
+};
+
+const Item=new mongose.model("item",itemSchema);
+
+const homeStartingContent = "Welcome to DAILY JOURNAL. You can use this website to post your Blogs on different subject matter. For any changes or inconvenience feel free to contact. Happy Blogging.";
 
 const app = express();
 
-let posts=[];
+const posts=[];
+let contacts=[];
 let year=new Date().getFullYear();
 
 app.set('view engine', 'ejs');
@@ -21,17 +29,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-  res.render("home",{year:year,homeContent:homeStartingContent,posts:posts});
-  
+  Item.find({},(err,listItems)=>{
+    res.render("home",{year:year,homeContent:homeStartingContent,listItems:listItems});
+  })
 })
 
 app.get("/about",(req,res)=>{
-  res.render("about",{year:year,aboutContent:aboutStartingContent});
+  res.render("about",{year:year});
   
 })
 
 app.get("/contact",(req,res)=>{
-  res.render("contact",{year:year,contactContent:contactStartingContent});
+  res.render("contact",{year:year});
 })
 
 app.get("/compose",(req,res)=>{
@@ -48,19 +57,41 @@ app.post("/compose",(req,res)=>{
     title:req.body.journalTitle,
     post:req.body.journalPost
   };
-  posts.push(post);
+  const item=new Item({
+    title:post.title,
+    body:post.post
+  });
+  item.save();
   res.redirect("/");
 })
 
 app.get("/post/:userPost",(req,res)=>{
-  let title=_.lowerCase(req.params.userPost);
-  for(let i=0;i<posts.length;++i)
-  {
-    let postsTitle=_.lowerCase(posts[i].title);
-    if(postsTitle===title){
-      res.render("post",{year:year,element:posts[i]})
-    }
-  }
+  let title=req.params.userPost;
+  Item.findById(title,(err,listItem)=>{
+    if(err)
+      console.log(err);
+    else
+      res.render("post",{year:year,element:listItem})
+  })
+})
+
+app.post("/delete",(req,res)=>{
+  const toBeDeletedItemId=req.body.button;
+  Item.findOneAndRemove(toBeDeletedItemId,(err,docs)=>{
+    if(err)
+        console.log(err);
+  });
+  res.redirect("/");
+})
+
+app.post("/contact",(req,res)=>{
+  const name=req.body.name;
+  const mail=req.body.email;
+  const suggestion=req.body.suggestion;
+  let contact={fname:name,email:mail,sug:suggestion};
+  contacts.push(contact);
+  console.log(name+" "+mail+" "+suggestion);
+  res.redirect("/");
 })
 
 app.listen(3000, function() {
